2dc9eb0df1a45bc22f5ebcebb127215165de881c
KinetekEnergy <transyt8@gmail.com>
2022-09-28 22:04:01 -0700

    Moved everything from old repo to new
    - buncha problems with old repo
    - created fresh one to try and fix all the broken stuff
    - hope this works

---
+++ Week2/Arrays/arrays.java
@@ -0,0 +1,15 @@
+public class arrays {
+    public static void main(String[] args) {
+                
+        String[] arr = {"Software", "Jeanne Calment", "Not sure on this one", "Deep Space"};
+
+        /*
+            element containing the name of your department
+            element contianing the name of the oldest person in the world
+            - An element contianing the name of the Rapid React world champion alliance captains
+            - An element containing the name of the 2019 FRC game
+
+            - Then print each element, from last to first.
+        */
+    }
+}
