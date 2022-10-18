872de093d2b9b03ffbf8994faea26146139b9259
KinetekEnergy <transyt8@gmail.com>
2022-10-17 22:11:13 -0700

    Update classes_objects.java

--- Week3/Classes & Objects/classes_objects.java
+++ Week3/Classes & Objects/classes_objects.java
@@ -1,37 +1,6 @@
-import java.util.*;
-
-public class classes_objects {
-    public class Me
-    {
-        String name;
-        int age;
-
-        static void name ()
-        {
-            System.out.println(name);
-        }
-
-        static void age()
-        {
-            System.out.println(age);
-        }
-
-        public static void main(String[] args) {
-            Me myObj = new Me();
-
-            myObj.name = "walter";
-            myObj.age = 40;
-
-            name();
-            age();
-    
-            /*
-             * Create an object for this class in the main method, & use the method to
-             * print!
-             */
-        }
+public class classes_objects
+{
+    public static void main(String[] args) {
+        
     }
-    
-
-    
 }
